097ef8e0c6f2c0cf3f4ab12c591e450b914c13bd
haris.tiric <haris.harisaga@gmail.com>
2023-11-27 01:29:36 +0100

    improvements

--- migrate.py
+++ migrate.py
@@ -1,20 +1,31 @@
-import cv2
 import os
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from google.auth.transport.requests import Request
+from google.oauth2.service_account import Credentials
 
-path = './data/'
+folder_id = '1xenMtBaq7Gww4TayFftQGhHAPBksA2oZ'
 
-def load_images_from_folder(path):
-    images = []
-    for filename in os.listdir(path):
-        img = cv2.imread(os.path.join(path,filename))
-        if img is not None:
-            images.append(img)
-    return images
+credentials = Credentials.from_service_account_file('./client_secrets.json', scopes=['https://www.googleapis.com/auth/drive.file'])
 
-# array of cats
-cats = []
+service = build('drive', 'v3', credentials=credentials)
 
-cats = load_images_from_folder(path)
+def upload_image(file_path, folder_id):
+    file_name = os.path.basename(file_path)
+    
+    file_metadata = {
+        'name': file_name,
+        'parents': [folder_id]
+    }
 
-for i in cats:
-    print(i)
+    media = MediaFileUpload(file_path, resumable=True)
+    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+
+    print(f'File ID: {file.get("id")}, File Name: {file_name} uploaded to folder ID: {folder_id}')
+
+image_folder_path = './data/'
+
+for filename in os.listdir(image_folder_path):
+    if filename.endswith('.jpg') or filename.endswith('.png'): 
+        file_path = os.path.join(image_folder_path, filename)
+        upload_image(file_path, folder_id)
